9fdbc28e6eb88f5c56ce14e66cf063c2f852411c
elfahad786 <50772061+elfahad786@users.noreply.github.com>
2019-05-18 17:13:27 +0400

    first commit

---
+++ mon-site/src/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    // open modal
+    $( '#open_modal').click(function() {
+        $('#modal_to_open').css(
+            {
+                'display': 'block'
+            }
+        );
+    });
+
+    // close modal
+    $('#close_modal').click(function() {
+        $('#modal_to_open').css(
+            {
+                'display': 'none'
+            }
+            
+        )
+    });
+
+    //send mail with ajax 
+});
